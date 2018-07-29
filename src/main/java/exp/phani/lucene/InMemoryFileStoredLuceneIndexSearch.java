cfcd617b985819d59f0e947de6733270c5cff6ac
N T R Phani Kumar <Phani.Kumar@spx.com>
2018-07-29 09:46:53 +0530

    Inmemory index search with filesystem example also added

--- src/main/java/exp/phani/lucene/InMemoryFileStoredLuceneIndexSearch.java
+++ src/main/java/exp/phani/lucene/InMemoryFileStoredLuceneIndexSearch.java
@@ -5,15 +5,15 @@ import java.nio.file.Paths;
 
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
 
-public class PersistedLuceneIndexSearch extends AbstractLuceneIndexSearch {
-    
-    public PersistedLuceneIndexSearch() throws IOException {
-        super(FSDirectory.open(Paths.get("_index/2")));
+public class InMemoryFileStoredLuceneIndexSearch extends AbstractLuceneIndexSearch {
+    public InMemoryFileStoredLuceneIndexSearch() throws IOException {
+        super(new RAMDirectory(FSDirectory.open(Paths.get("_index/1")), null));
     }
     
     public static void main(String[] args) throws IOException, ParseException {
-        LuceneIndexSearch luceneIndexSearch = new PersistedLuceneIndexSearch();
+        LuceneIndexSearch luceneIndexSearch = new InMemoryFileStoredLuceneIndexSearch();
         luceneIndexSearch.index("Lucene in Action", "193398817");
         luceneIndexSearch.index("Lucene for Dummies", "55320055Z");
         luceneIndexSearch.index("Managing Gigabytes", "55063554A");
